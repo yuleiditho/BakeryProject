ae5aa019ef501a7cace8fb4304c9e97d6d776934
Denisse <yuleiditho@gmail.com>
2024-12-12 21:36:38 -0600

    style: Implement new sections: Faq & Footer
    -Add medias queries for responsive design
    -Style menu toggle button and navbar
    -Style the Faq
    -Style the footer
    -Style the button chat

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -91,3 +91,26 @@
     init();
     body.appendChild(canvas);
 })();
+
+
+//questions
+document.addEventListener('DOMContentLoaded', () => {
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(item => {
+        const question = item.querySelector('.faq-question');
+
+        question.addEventListener('click', () => {
+            // Alternar la clase 'open' para abrir/cerrar
+            item.classList.toggle('open');
+
+            // Cerrar los otros ítems si están abiertos
+            faqItems.forEach(otherItem => {
+                if (otherItem !== item) {
+                    otherItem.classList.remove('open');
+                }
+            });
+        });
+    });
+});
+
